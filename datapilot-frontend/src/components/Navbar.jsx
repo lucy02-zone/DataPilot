b94d3d58be111053facc78116c5aaeca02019aa1
Lakshmi Prasanna <lakshmiprasannareddy849@gmail.com>
2026-07-17 20:05:55 +0530

    navbar added

---
+++ datapilot-frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+
+  return (
+
+    <div className="navbar">
+
+      <h2>DataPilot Analytics Platform</h2>
+
+      <button>
+        Logout
+      </button>
+
+    </div>
+
+  );
+}
+
+export default Navbar;
